4c950b47803672e594cac9801f42adc7eb481975
Daniel Roberts <roda0313@hotmail.com>
2018-02-26 13:41:28 -0500

    Added better logging

--- GarageDoorMain.cpp
+++ GarageDoorMain.cpp
@@ -1,17 +1,24 @@
 #include <cstdlib>
 #include <iostream>
 #include <pthread.h>
+#include <sys/neutrino.h>
+#include <sys/netmgr.h>
+#include <unistd.h>
 
 #include "Machine.h"
 
 void *RunInputScaner(void* args) {
 
+	std::cout << "Input Scanner running..." << std::endl;
+
+	std::cout << "Enter m for OVERCURRENT, i for IR_TRIP, r for PUSH_BUTTON then push enter." << std::endl;
+	std::cout << "Enter s to shutdown the machine..." << std::endl;
+
 	char inp;
 	while(1)
 	{
-		std::cout << "Enter m for OVERCURRENT, i for IR_TRIP, r for PUSH_BUTTON then push enter..." << std::endl;
-		std::cout << "Enter s to shutdown the machine..." << std::endl;
 		std::cin >> inp;
+		std::cout << "GOT " << inp << std::endl;
 
 		switch(inp)
 		{
@@ -27,7 +34,7 @@ void *RunInputScaner(void* args) {
 		case 's':
 			return EXIT_SUCCESS;
 		default:
-			std::cout << "Invalid input" << inp << std::endl;
+			std::cout << "Invalid input " << inp << std::endl;
 		}
 	}
 
@@ -37,11 +44,18 @@ void *RunInputScaner(void* args) {
 void *RunController(void* args) {
 	Controller::Machine m1 = Controller::Machine();
 
+	std::cout << "Controller running..." << std::endl;
+	while(1){
+		sleep(1);
+	}
+
 	return EXIT_SUCCESS;
 }
 
 
 int main(int argc, char *argv[]) {
+
+	std::cout << "Main running... " << std::endl;
 
 	pthread_t inputScannerThread;
 	pthread_t controllerThread;
@@ -50,7 +64,7 @@ int main(int argc, char *argv[]) {
 	pthread_create( &controllerThread, NULL, &RunController, (void*)&inputScannerThread );
 
 	pthread_join(inputScannerThread, NULL);
-	pthread_join(controllerThread, NULL);
+	pthread_kill(controllerThread, 0);
 
 	return EXIT_SUCCESS;
 }
