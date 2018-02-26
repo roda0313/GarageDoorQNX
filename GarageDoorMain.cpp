ecf52bb2b1b06e9e89718774687f377927cede77
Daniel Roberts <roda0313@hotmail.com>
2018-02-26 12:21:27 -0500

    Added basic threading model

--- GarageDoorMain.cpp
+++ GarageDoorMain.cpp
@@ -1,33 +1,56 @@
 #include <cstdlib>
 #include <iostream>
+#include <pthread.h>
 
 #include "Machine.h"
 
-int main(int argc, char *argv[]) {
-
-	Controller::Machine m1 = Controller::Machine();
+void *RunInputScaner(void* args) {
 
 	char inp;
 	while(1)
 	{
-		std::cout << "Enter m, i or r then push enter..." << std::endl;
+		std::cout << "Enter m for OVERCURRENT, i for IR_TRIP, r for PUSH_BUTTON then push enter..." << std::endl;
+		std::cout << "Enter s to shutdown the machine..." << std::endl;
 		std::cin >> inp;
 
 		switch(inp)
 		{
 		case 'm':
-			m1.HandleEvent(Controller::OVERCURRENT);
+//			m1.HandleEvent(Controller::OVERCURRENT);
 			break;
 		case 'i':
-			m1.HandleEvent(Controller::IR_TRIP);
+//			m1.HandleEvent(Controller::IR_TRIP);
 			break;
 		case 'r':
-			m1.HandleEvent(Controller::PUSH_BUTTON);
+//			m1.HandleEvent(Controller::PUSH_BUTTON);
 			break;
+		case 's':
+			return EXIT_SUCCESS;
 		default:
 			std::cout << "Invalid input" << inp << std::endl;
 		}
 	}
+
+	return EXIT_SUCCESS;
+}
+
+void *RunController(void* args) {
+	Controller::Machine m1 = Controller::Machine();
+
+	return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char *argv[]) {
+
+	pthread_t inputScannerThread;
+	pthread_t controllerThread;
+
+	pthread_create( &inputScannerThread, NULL, &RunInputScaner, (void*)&controllerThread );
+	pthread_create( &controllerThread, NULL, &RunController, (void*)&inputScannerThread );
+
+	pthread_join(inputScannerThread, NULL);
+	pthread_join(controllerThread, NULL);
 
 	return EXIT_SUCCESS;
 }
